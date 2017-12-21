f81f50ba19eb8b7399459cfbf3e9ac24a65e7d39
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-12-21 20:27:36 -0200

    Fix typo

--- client/src/common/template/header.jsx
+++ client/src/common/template/header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default props => (
   <header className="main-header">
     <a href="#" className="logo">
-      <span className="logo-mini"><b>My</b>M</span>
+      <span className="logo-mini"><i className="fa fa-money"></i></span>
       <span className="logo-lg">
         <i className="fa fa-money"></i>
-        <b>My</b>Money
+        <b> Payment</b>ADM
       </span>
     </a>
     <nav className="navbar navbar-static-top">
--- client/src/common/template/menu.jsx
+++ client/src/common/template/menu.jsx
@@ -6,7 +6,7 @@ import MenuTree from './menuTree'
 export default props => (
   <ul className="sidebar-menu">
     <MenuItem path="#" label="Dashboard" icon="dashboard" />
-    <MenuTree label="Register" icon="edit" >
+    <MenuTree label="Registry" icon="edit" >
       <MenuItem path="#billingCycles" label="Billing Cicles" icon="usd" />
     </MenuTree>
   </ul>
