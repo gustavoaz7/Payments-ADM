d322551edf787d706061059a92d0196d6decae89
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-12-21 21:02:33 -0200

    Config dashboard component

---
+++ client/src/common/layout/grid.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Grid extends Component {
+  toCssClasses(nums) {
+    const cols = nums ? nums.split(" ") : []
+    let classes = ""
+
+    if(cols[0]) classes += `col-xs-${cols[0]}`
+    if(cols[1]) classes += ` col-sm-${cols[1]}`
+    if(cols[2]) classes += ` col-md-${cols[2]}`
+    if(cols[3]) classes += ` col-lg-${cols[3]}`
+
+    return classes
+  }
+
+  render() {
+    return (
+      <div className={this.toCssClasses(this.props.cols || 12)}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
--- client/src/dashboard/dashboard.jsx
+++ client/src/dashboard/dashboard.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-  <div>
-    <h1>Dashboard</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Row from '../common/layout/row'
+import ValueBox from '../common/widget/valueBox'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Dashboard" small="Version 1.0" />
+        <Content>
+          <Row>
+          <ValueBox cols="12 4" color="green" icon="bank"
+            value="10.00" text="Total Credit Balance"
+          />
+          <ValueBox cols="12 4" color="red" icon="credit-card"
+            value="8.00" text="Total Debt Balance"
+          />
+          <ValueBox cols="12 4" color="blue" icon="money"
+            value="2.00" text="Total Cash Balance"
+          />
+          </Row>
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
