656d57ca40450e3b981057d00ace515310c155fa
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-12-23 20:18:10 -0200

    Add BillingCycleForm with redux-form

--- client/src/billingCycle/billingCycle.jsx
+++ client/src/billingCycle/billingCycle.jsx
@@ -4,13 +4,17 @@ import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
-import List from '../billingCycle/billingCycleList'
+
+import List from './billingCycleList'
+import Form from './billingCycleForm'
+import { create } from './billingCycleActions'
 
 class BillingCycle extends Component {
 
@@ -35,7 +39,9 @@ class BillingCycle extends Component {
               <TabContent id="tabList"> <h1>List</h1> 
                 <List />
               </TabContent>
-              <TabContent id="tabCreate"> <h1>Add</h1> </TabContent>
+              <TabContent id="tabCreate"> <h1>Add</h1> 
+                <Form onSubmit={this.props.create}/>
+              </TabContent>
               <TabContent id="tabUpdate"> <h1>Update</h1> </TabContent>
               <TabContent id="tabDelete"> <h1>Delete</h1> </TabContent>
             </TabsContent>
@@ -46,6 +52,8 @@ class BillingCycle extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({
+  selectTab, showTabs, create
+}, dispatch)
 
 export default connect(null, mapDispatchToProps)(BillingCycle)
--- client/src/billingCycle/billingCycleActions.js
+++ client/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -7,5 +8,19 @@ export function getList() {
   return {
     type: 'BILLING_CYCLES_FETCHED',
     payload: req
+  }
+}
+
+export function create(values) {
+  axios.post(`${BASE_URL}/billingCycles`, values)
+  .then(res => {
+    toastr.success('Success', 'Task successfully completed!')
+  })
+  .catch(e => {
+    // We've setted `errors` on server-side confir
+    e.response.data.errors.forEach(err => toastr.error('Error ', err))
+  })
+  return {
+    type: 'testing'
   }
 }
