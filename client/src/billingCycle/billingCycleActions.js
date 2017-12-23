819b03589691eac2f65814a984259ef8001cd919
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-12-23 20:45:06 -0200

    Improve form response on submit and field styling with separate component

--- client/src/billingCycle/billingCycleActions.js
+++ client/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -12,15 +14,21 @@ export function getList() {
 }
 
 export function create(values) {
-  axios.post(`${BASE_URL}/billingCycles`, values)
-  .then(res => {
-    toastr.success('Success', 'Task successfully completed!')
-  })
-  .catch(e => {
-    // We've setted `errors` on server-side confir
-    e.response.data.errors.forEach(err => toastr.error('Error ', err))
-  })
-  return {
-    type: 'testing'
+  return dispatch => {
+    axios.post(`${BASE_URL}/billingCycles`, values)
+    .then(res => {
+      toastr.success('Success', 'Task successfully completed!')
+      // middleware `multi` allows to pass in an array to dispatch
+      dispatch([
+        resetForm('billingCycleForm'),
+        getList(),
+        selectTab('tabList'),
+        showTabs('tabList', 'tabCreate')
+      ])
+    })
+    .catch(e => {
+      // We've setted `errors` on server-side confir
+      e.response.data.errors.forEach(err => toastr.error('Error ', err))
+    })
   }
 }
--- client/src/billingCycle/billingCycleForm.jsx
+++ client/src/billingCycle/billingCycleForm.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 
+import labelAndInput from '../common/form/labelAndInput'
+
 class BillingCycleForm extends Component {
   render() {
     const { handleSubmit } = this.props
@@ -8,9 +10,15 @@ class BillingCycleForm extends Component {
     return (
       <form role="role" onSubmit={handleSubmit}>
         <div className="box-body">
-          <Field name="name" component="input" />
-          <Field name="month" component="input" />
-          <Field name="year" component="input" />
+          <Field name="name" component={labelAndInput} 
+            label="Name" cols='12 4' placeholder="Name"
+          />
+          <Field name="month" component={labelAndInput} 
+            label="Month" cols='12 4' placeholder="Month" type="number"
+          />
+          <Field name="year" component={labelAndInput} 
+            label="Year" cols="12 4" placeholder="Year" type="number"
+          />
         </div>
         <div className="box-footer">
           <button type="submit" className="btn btn-primary">Submit</button>
